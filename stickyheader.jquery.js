2c7875b8284db0eb76de56c26e8fec49fca2aa73
Farid Rener <proteusvacuum@users.noreply.github.com>
2014-07-21 18:05:25 -0400

    Can now stick, unstick, and restick.
    Usage: 
    $table.stickyHeader({mode: "stick"})
    $table.stickyHeader({mode: "unstick"})
    $table.stickyHeader({mode: "restick"})
    
    Useful for when you are dynamically updating the dom and your table columns are changing size.

--- stickyheader.jquery.js
+++ stickyheader.jquery.js
@@ -1,60 +1,76 @@
 
 jQuery.fn.extend({
-	stickHeader: function(offset){
-		var tables = this;
-		this.each(function(i){
-			var table = tables[i];
-			var tableClone = $(table).clone(true).empty().removeClass('stickyHeader');
-			var theadClone = $(table).find('thead').clone(true);
-			var stickyHeader =  $('<div></div>').addClass('stickyHeader hide').attr('aria-hidden', 'true');
-			stickyHeader.append(tableClone).find('table').append(theadClone);
-			$(table).after(stickyHeader);
+	stickyHeader: function( options ){
+        var settings = $.extend({
+            mode 	: "stick",
+            offset 	: 0,
+            allowCutOffBottom: false
+        }, options );
+        
+        var tables = this; 
+        var stickyHeader, cutoffTop, cutoffBottom
+        
+        var windowScroll = function(){
+			var currentPosition = $(window).scrollTop();
 
-			var tableHeight = $(table).height();
-			var tableWidth = $(table).width() + Number($(table).css('padding-left').replace(/px/ig,"")) + Number($(table).css('padding-right').replace(/px/ig,"")) + Number($(table).css('border-left-width').replace(/px/ig,"")) + Number($(table).css('border-right-width').replace(/px/ig,""));
-
-			var headerCells = $(table).find('thead th');
-			var headerCellHeight = $(headerCells[0]).height();
-
-			var no_fixed_support = false;
-			if (stickyHeader.css('position') == "absolute") {
-				no_fixed_support = true;
+			if ( currentPosition > cutoffTop && currentPosition < cutoffBottom ) {
+				stickyHeader.removeClass('hide');
+				stickyHeader.css('top', settings.offset + 'px');
 			}
-
-			var stickyHeaderCells = stickyHeader.find('th');
-			stickyHeader.css('width', tableWidth);
-			var cellWidths = [];
-			for (var i = 0, l = headerCells.length; i < l; i++) {
-				cellWidths[i] = $(headerCells[i]).innerWidth();
+			else {
+				stickyHeader.addClass('hide');
 			}
-			for (var i = 0, l = headerCells.length; i < l; i++) {
-				$(stickyHeaderCells[i]).css('width', cellWidths[i]);
-			}
+        };
 
-			var cutoffTop = $(table).offset().top;
-			var cutoffBottom = tableHeight + cutoffTop - headerCellHeight;
+        if (settings.mode == "stick") {
+        	return tables.each(function(i){
+				var table = tables[i];
+				var tableClone = $(table).clone(true).empty().removeClass('stickyHeader');
+				var theadClone = $(table).find('thead').clone(true);
+				stickyHeader =  $('<div></div>').addClass('stickyHeader hide').attr('aria-hidden', 'true');
+				stickyHeader.append(tableClone).find('table').append(theadClone);
+				$(table).after(stickyHeader);
 
-			jQuery.fn.stickHeader.windowScroll = function() {
-				var currentPosition = $(window).scrollTop();
-				if (currentPosition > cutoffTop && currentPosition < cutoffBottom) {
-					stickyHeader.removeClass('hide');
-					if (no_fixed_support) {
-						stickyHeader.css('top', currentPosition + 'px');
-					}
-					if (offset){
-						stickyHeader.css('top', offset + 'px');
-					}
+				var tableHeight = $(table).height();
+				var tableWidth = $(table).width() + Number($(table).css('padding-left').replace(/px/ig,"")) + Number($(table).css('padding-right').replace(/px/ig,"")) + Number($(table).css('border-left-width').replace(/px/ig,"")) + Number($(table).css('border-right-width').replace(/px/ig,""));
+
+				var headerCells = $(table).find('thead th');
+				var headerCellHeight = $(headerCells[0]).height();
+
+				var stickyHeaderCells = stickyHeader.find('th');
+				stickyHeader.css('width', tableWidth);
+				var cellWidths = [];
+				for (var i = 0, l = headerCells.length; i < l; i++) {
+					cellWidths[i] = $(headerCells[i]).innerWidth();
 				}
-				else {
-					stickyHeader.addClass('hide');
+				for (var i = 0, l = headerCells.length; i < l; i++) {
+					$(stickyHeaderCells[i]).css('width', cellWidths[i]);
 				}
-			}
 
-			$(window).on("scroll", jQuery.fn.stickHeader.windowScroll);
+				cutoffTop = $(table).offset().top;
+				cutoffBottom = settings.allowCutOffBottom ? tableHeight + cutoffTop - headerCellHeight : 999999;
 
-			jQuery.fn.stickHeader.unstick = function(){
-				$(window).off("scroll", jQuery.fn.stickHeader.windowScroll)
-			}
-		});
-}
+				$(window).on( "scroll", windowScroll );
+
+
+        	});
+        }
+
+        else if (settings.mode == "unstick"){
+        	return tables.each(function(i){
+        		$('div.stickyHeader').remove()
+				$(window).off( "scroll", windowScroll );
+        	});        	
+        }
+
+        else if (settings.mode == "restick"){
+        	return tables.each(function(i){
+        		$('div.stickyHeader').remove()
+        		$(window).off( "scroll", windowScroll );
+        		tables.stickyHeader({ mode: "stick", offset: settings.offset, allowCutOffBottom: settings.allowCutOffBottom }).scroll()
+        	});
+        }
+
+
+	}
 });
